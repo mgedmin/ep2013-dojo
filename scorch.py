7744fc235f7b3907c39e9ec9dace3ac05ff40c17
Marius Gedminas <marius@gedmin.as>
2013-07-05 18:59:50 +0200

    IT WORKS

--- scorch.py
+++ scorch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import math
+import random
 import pygame
 from pygame.locals import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_SPACE, K_UP, K_DOWN
 
@@ -42,8 +43,8 @@ class Tank(object):
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color if self.alive else DEAD_TANK_COLOR,
-                           (self.x, self.y), 7)
-        if self.active and self.alive:
+                           map(int, (self.x, self.y)), 7)
+        if self.active:
             pygame.draw.line(surface, self.color,
                              map(int, self.rel_pos(10)),
                              map(int, self.rel_pos(10 + self.magnitude)))
@@ -51,15 +52,17 @@ class Tank(object):
         ##                   (self.x - 20, self.y - 20, 40, 40), 1)
 
     def shoot(self):
-        return Bullet(*(self.rel_pos(10) + self.vector(self.magnitude)))
+        return Bullet(*(self.rel_pos(COLLISION_RADIUS + 1) + self.vector(self.magnitude)),
+                      color=self.color)
 
 
 class Bullet(object):
-    def __init__(self, x, y, dx=0, dy=0):
+    def __init__(self, x, y, dx=0, dy=0, color=None):
         self.x = self.old_x = x
         self.y = self.old_y = y
         self.dx = dx
         self.dy = dy
+        self.color = color
 
     def update(self):
         self.old_x = self.x
@@ -88,33 +91,80 @@ class Bullet(object):
         return False
 
 
-def collides(bullet, tank):
-    return math.hypot(tank.x - bullet.x, tank.y - bullet.y) < COLLISION_RADIUS
+class Game(object):
+    def __init__(self):
+        y1 = random.randrange(300-30, 300+30)
+        y2 = random.randrange(300-30, 300+30)
+
+        x1 = random.randrange(20, 300)
+        x2 = 1024 - random.randrange(20, 300)
+
+        colors = [RED, GREEN]
+        random.shuffle(colors)
+        self.tank1 = Tank(x1, y1 + (y2 - y1) * x1 / 1024.0,
+                          color=colors[0])
+        self.tank2 = Tank(x2, y1 + (y2 - y1) * x2 / 1024.0,
+                          color=colors[1], direction=45+90)
+        self.ground = [(0, y1), (1024, y2)]
+        self.tanks = [self.tank1, self.tank2]
+        self.bullets = []
+        self.current_tank = self.tank1
+        self.font = pygame.font.Font(None, 48)
+        self.message = self.font.render("Go go go!", True, (255, 255, 255))
+        self.message_ttl = FRAMERATE * 5
+        self.over = False
+
+    def draw(self, screen):
+        if self.message:
+            screen.blit(self.message, ((screen.get_width() - self.message.get_width())//2, 20))
+            self.message_ttl -= 1
+            if self.message_ttl < 0:
+                self.message = None
+                self.message_ttl = FRAMERATE * 5
+        pygame.draw.line(screen, (255, 255, 255), *self.ground)
+        for tank in self.tanks:
+            tank.active = tank is self.current_tank
+            tank.draw(screen)
+        for bullet in self.bullets:
+            bullet.draw(screen)
+
+    def update(self):
+        live_bullets = []
+        for bullet in self.bullets:
+            if bullet.y > max(y for x, y in self.ground):
+                continue
+            bullet.update()
+            for tank in self.tanks:
+                if bullet.collides_with(tank):
+                    tank.explode()
+                    if bullet.color == tank.color: # aka suicide
+                        self.message = self.font.render("YOU LOSE!", True, bullet.color)
+                    else:
+                        self.message = self.font.render("YOU WIN!", True, bullet.color)
+                    self.over = True
+                    continue # bullet is gone gone gone
+            live_bullets.append(bullet)
+        self.bullets = live_bullets
+
+    def shoot(self):
+        self.bullets.append(self.current_tank.shoot())
+        # hardcoded for two tanks
+        self.current_tank = self.tanks[1 - self.tanks.index(self.current_tank)]
 
 
 def main():
     pygame.init()
     pygame.display.set_caption('Py Scorched Earth')
+
     screen = pygame.display.set_mode((1024, 600))
-
-    tank1 = Tank(30, 290)
-    tank2 = Tank(900, 243, direction=45+90, color=GREEN)
-    tanks = [tank1, tank2]
-    bullets = []
-
-    current_tank = tank1 # focus tracking ;)
-
     clock = pygame.time.Clock()
+
+    game = Game()
 
     while True:
         # draw
         screen.fill((0, 0, 0))
-        pygame.draw.line(screen, (255, 255, 255), (20, 300), (1000, 250))
-        for tank in tanks:
-            tank.active = tank is current_tank
-            tank.draw(screen)
-        for bullet in bullets:
-            bullet.draw(screen)
+        game.draw(screen)
         pygame.display.flip()
         # process events
         for event in pygame.event.get():
@@ -123,33 +173,27 @@ def main():
             elif event.type == KEYDOWN:
                 if event.unicode in ('q', 'Q'):
                     return
-                if event.key == K_SPACE:
-                    bullets.append(current_tank.shoot())
-                    # hardcoded for two tanks
-                    current_tank = tanks[1 - tanks.index(current_tank)]
+                if event.unicode in ('n', 'N'):
+                    game = Game()
+                if event.key == K_SPACE and not game.over:
+                    game.shoot()
 
-        if pygame.key.get_pressed()[K_UP]:
-            current_tank.magnitude += MAG_SPEED
-            current_tank.magnitude = min(current_tank.magnitude, MAX_MAGNITUDE)
-        if pygame.key.get_pressed()[K_DOWN]:
-            current_tank.magnitude -= MAG_SPEED
-            current_tank.magnitude = max(current_tank.magnitude, MIN_MAGNITUDE)
-        if pygame.key.get_pressed()[K_LEFT]:
-            current_tank.direction += TURN_SPEED
-        if pygame.key.get_pressed()[K_RIGHT]:
-            current_tank.direction -= TURN_SPEED
+        if not game.over:
+            current_tank = game.current_tank
+            if pygame.key.get_pressed()[K_UP]:
+                current_tank.magnitude += MAG_SPEED
+                current_tank.magnitude = min(current_tank.magnitude, MAX_MAGNITUDE)
+            if pygame.key.get_pressed()[K_DOWN]:
+                current_tank.magnitude -= MAG_SPEED
+                current_tank.magnitude = max(current_tank.magnitude, MIN_MAGNITUDE)
+            if pygame.key.get_pressed()[K_LEFT]:
+                current_tank.direction += TURN_SPEED
+            if pygame.key.get_pressed()[K_RIGHT]:
+                current_tank.direction -= TURN_SPEED
         # move
-        live_bullets = []
-        for bullet in bullets:
-            if bullet.y > screen.get_height():
-                continue
-            bullet.update()
-            for tank in tanks:
-                if bullet.collides_with(tank):
-                    tank.explode()
-                    continue # bullet is gone gone gone
-            live_bullets.append(bullet)
-        bullets = live_bullets
+        game.update()
+        if game.over and game.message is None:
+            game = Game()
         # wait
         clock.tick(FRAMERATE)
 
